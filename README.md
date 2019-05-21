c1983969e95efb1f2df8180ab2b1a7d972ee639a
Jason Fayre <jfayre@gmail.com>
2019-05-21 07:23:50 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# voice flight following
+Periodically read the name of the closest city to your aircraft.
+
+## Requirements
+* Python 2.7 - 32 bit (due to pyuipc incompatibility with Python 3 and 64 bit)
+* pywin32
+* XPlane with [XPUIPC](http://fsacars.com/downloads/xpuipc/) or MFS, P3D with [FSUIPC](http://www.schiratti.com/dowson.html) or P3D
+* Windows
+    * XPUIPC currently not working with Linux and Mac
+
+## Installation
+* Get the latest python 2.7 ([Python releases](https://www.python.org/downloads/))
+* Install the latest pywin32 release ([pywin32 releases](https://github.com/mhammond/pywin32/releases))
+    * filename: `pywin32-xxx.win32-py2.7.exe`
+    * Install with the installer, not using pip!
+
+
+## Usage
+* Start your sim and start a flight.
+* Start the script "FlightFollowing.py" 
+* You should hear the name of the closest city to your aircraft location
+
+## Bugs and issues
+* Please report bugs via the github issues tab.
+    * It is usefull to attach the logfile from "root/logs".
+    
+### Known limitations
+* The city given does not take into account the heading of your aircraft. So, the nearest city may be behind you.
+* Offline access is not implemented yet. It will be added in the future.
+
+## Used packages and Copyright
+
+### pyttsx
+Text-to-speech package for python. Used to read the parsed ATIS string.
+
+pyttsx Copyright (c) 2009, 2013 Peter Parente
+
+Permission to use, copy, modify, and distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+
+### pyuipc - FSUIPC SDK for Python
+Used to get the com frequencies, com status, aircraft coordinates from the simulator.
+
+All Copyright - Peter Dowson and István Váradi.
+
+
+## Changelog
---
+++ flightFollowing.py
@@ -0,0 +1,282 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+#==============================================================================
+# Voice Flight Following - Periodically announce cities along your flight path
+# Copyright (C) 2019 by Jason Fayre
+# based on the VoiceATIS addon by   Oliver Clemens
+# 
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.	 See the GNU General Public License for more
+# details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# this program.	 If not, see <https://www.gnu.org/licenses/>.
+#==============================================================================
+
+from __future__ import division
+
+# Import built-ins
+import os
+import sys
+import re
+import time
+import urllib
+import urllib2
+import gzip
+from contextlib import closing
+from math import *
+import warnings
+import json
+from ConfigParser import SafeConfigParser
+
+# Set encoding
+reload(sys)
+sys.setdefaultencoding('iso-8859-15')  # @UndefinedVariable
+
+# Import pip packages (except failure with debug mode).
+try:
+	import pyttsx
+	pyttsxImported = True
+except ImportError:
+	pyttsxImported = False
+
+try:
+	import pyuipc
+	pyuipcImported = True
+	debug = False
+except ImportError:
+		pyuipcImported = False
+		debug = True
+
+
+from aviationFormula.aviationFormula import calcBearing
+# from aviationFormula import gcDistanceNm
+
+# Import own packages.
+from VaLogger import VaLogger
+# from voiceAtisUtil import parseVoiceInt, parseVoiceString, CHAR_TABLE
+
+## Main Class of FlightFollowing.
+# Run constructor to run the program.
+class FlightFollowing(object):
+	OFFSETS = [(0x034E,'H'),	# com1freq
+			   (0x3118,'H'),	# com2freq
+			   (0x3122,'b'),	# radioActive
+			   (0x0560,'l'),	# ac Latitude
+			   (0x0568,'l'),	# ac Longitude
+			  ]
+	
+	
+
+	
+
+	## Setup the FlightFollowing object.
+	# Also starts the voice generation loop.
+	def __init__(self,**optional):
+		#TODO: Remove the debug code when tested properly.
+		
+		# Process optional arguments.
+		self.debug = optional.get('Debug',debug)
+		
+		# Get file path.
+		self.rootDir = os.path.abspath(os.path.dirname(sys.argv[0]))
+		
+		# Init logging.
+		self.logger = VaLogger(os.path.join(self.rootDir,'voiceAtis','logs'))
+		# init config parser
+		self.config = SafeConfigParser()
+		# First log message.
+		self.logger.info('voiceAtis started')
+		# check for config file. Create it if it doesn't exist.
+		exists = os.path.isfile(self.rootDir + "/flightfollowing.ini")
+		if exists:
+			self.logger.info("config file exists.")
+			cfgfile = self.config.read(self.rootDir + "/flightfollowing.ini")
+			self.geonames_username = self.config.get('config','geonames_username')
+			self.interval = float(self.config.get('config','interval'))
+			self.distance_units = self.config.get('config','distance_units')
+			self.voice_rate = self.config.get('config','voice_rate')
+			
+			
+		else:
+			self.logger.info ("no config file found. It will be created.")
+			cfgfile = open (self.rootDir + "/flightfollowing.ini",'w')
+			self.config.add_section ('config')
+			self.config.set ('config','geonames_username','your_user_name')
+			self.config.set ('config','voice_rate','150')
+			self.config.set ('config','interval','180')
+			self.config.set ('config','distance_units','0')
+			
+			self.config.write(cfgfile)
+			cfgfile.close()
+			
+		# Establish pyuipc connection
+		while True:
+			try:
+				self.pyuipcConnection = pyuipc.open(0)
+				self.pyuipcOffsets = pyuipc.prepare_data(self.OFFSETS)
+				self.logger.info('FSUIPC connection established.')
+				break
+			except NameError:
+				self.pyuipcConnection = None
+				self.logger.warning('Using voiceAtis without FSUIPC.')
+				break
+			except:
+				self.logger.warning('FSUIPC: No simulator detected. Start you simulator first! Retrying in 20 seconds.')
+				time.sleep(20)
+		
+		# Show debug Info
+		#TODO: Remove for release.
+		if self.debug:
+			self.logger.info('Debug mode on.')
+			self.logger.setLevel(ConsoleLevel='debug')
+		
+		# Infinite loop.
+		try:
+			while True:
+				
+				# Get lat and lon from simulator
+				self.getPyuipcData()
+				# Lookup nearest cities to aircraft position using the Geonames database.
+				response = urllib2.urlopen('http://api.geonames.org/findNearbyPlaceNameJSON?lat={}&lng={}&username={}&cities=cities15000'.format(self.lat,self.lon, self.geonames_username))
+				data = json.load(response)
+
+				bearing = calcBearing (self.lat, self.lon, float(data["geonames"][0]["lat"]), float(data["geonames"][0]["lng"]))
+				bearing = (degrees(bearing) +360) % 360
+				if self.distance_units == 1:
+					distance = float(data["geonames"][0]["distance"]) / 1.609
+				else:
+					distance = float(data["geonames"][0]["distance"])
+				self.atisVoice='closest city: {}. Distance: {:.1f}. Bearing: {:.0f}'.format(data["geonames"][0]["name"],distance,bearing)
+				
+				self.airport="test"
+				# Read the string.
+				self.readVoice()
+				time.sleep(self.interval)
+				pass
+				
+		except KeyboardInterrupt:
+			# Actions at Keyboard Interrupt.
+			self.logger.info('Loop interrupted by user.')
+			if pyuipcImported:
+				self.pyuipc.close()
+			
+	
+	
+
+	## Reads the atis string using voice generation.
+	def readVoice(self):
+		# Init currently Reading with None.
+		self.currentlyReading = None
+		
+		self.logger.debug('Voice Text is: {}'.format(self.atisVoice))
+		
+		if pyttsxImported:
+			# Set properties currently reading
+			self.currentlyReading = self.airport
+			
+			# Init voice engine.
+			self.engine = pyttsx.init()
+			   
+			# Set properties.
+			voices = self.engine.getProperty('voices')
+			for vo in voices:
+				if 'english' in vo.name.lower():
+					self.engine.setProperty('voice', vo.id)
+					self.logger.debug('Using voice: {}'.format(vo.name))
+					break
+			
+			self.engine.setProperty('rate', self.voice_rate)
+			 
+			# Start listener and loop.
+			self.engine.connect('started-word', self.onWord)
+
+			# Say complete ATIS
+			self.engine.say(self.atisVoice)
+			self.logger.info('Start reading.')
+			self.engine.runAndWait()
+			self.logger.info('Reading finished.')
+			self.engine = None
+			
+		else:
+			self.logger.warning('Speech engine not initalized, no reading. Sleeping for {} seconds...'.format(self.SLEEP_TIME))
+			time.sleep(self.SLEEP_TIME)
+	
+	## Callback for stop of reading.
+	# Stops reading if frequency change/com deactivation/out of range.
+	def onWord(self, name, location, length):  # @UnusedVariable
+		self.getPyuipcData()
+		self.getAirport()
+		
+		if self.airport != self.currentlyReading:
+			self.engine.stop()
+			self.currentlyReading = None
+	
+	
+	## Reads current frequency and COM status.
+	def getPyuipcData(self):
+		
+		if pyuipcImported:
+			results = pyuipc.read(self.pyuipcOffsets)
+		
+			# frequency
+			hexCode = hex(results[0])[2:]
+			self.com1frequency = float('1{}.{}'.format(hexCode[0:2],hexCode[2:]))
+			hexCode = hex(results[1])[2:]
+			self.com2frequency = float('1{}.{}'.format(hexCode[0:2],hexCode[2:]))
+			
+			# radio active
+			#TODO: Test accuracy of this data (with various planes and sims)
+			radioActiveBits = list(map(int, '{0:08b}'.format(results[2])))
+			if radioActiveBits[2]:
+				self.com1active = True
+				self.com2active = True
+			elif radioActiveBits[0]:
+				self.com1active = True
+				self.com2active = False
+			elif radioActiveBits[1]:
+				self.com1active = False
+				self.com2active = True
+			else:
+				self.com1active = False
+				self.com2active = False
+			
+			# lat lon
+			self.lat = results[3] * (90.0/(10001750.0 * 65536.0 * 65536.0))
+			self.lon = results[4] * (360.0/(65536.0 * 65536.0 * 65536.0 * 65536.0))
+		
+		else:
+			self.com1frequency = self.COM1_FREQUENCY_DEBUG
+			self.com2frequency = self.COM2_FREQUENCY_DEBUG
+			self.com1active = True
+			self.com2active = True
+			self.lat = self.LAT_DEBUG
+			self.lon = self.LON_DEBUG
+		
+		# Logging.
+		if self.com1active:
+			com1activeStr = 'active'
+		else:
+			com1activeStr = 'inactive'
+		if self.com2active:
+			com2activeStr = 'active'
+		else:
+			com2activeStr = 'inactive'
+		
+		self.logger.debug('COM 1: {} ({}), COM 2: {} ({})'.format(self.com1frequency,com1activeStr,self.com2frequency,com2activeStr))
+		self.logger.debug('Latitude: {}, Longitude: {}'.format(self.lat, self.lon))
+
+
+
+#		  self.logger.debug('COM 1 active: {}, COM 2 active: {}'.format(self.com1active,self.com2active))
+	
+
+if __name__ == '__main__':
+	FlightFollowing = FlightFollowing()
+	pass
