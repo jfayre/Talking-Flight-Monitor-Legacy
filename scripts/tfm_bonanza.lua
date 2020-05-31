9a8b0fa2ee02791152570b5ea07979d996765370
Jason Fayre <jfayre@gmail.com>
2020-05-31 07:50:44 -0400

    more payload testing

--- scripts/tfm_bonanza.lua
+++ scripts/tfm_bonanza.lua
@@ -5,7 +5,7 @@
 function battery(varname, value)
     ipc.writeUB(0x66c0, value)
 end
--- event.Lvar("Battery1Switch",1000,"battery")
+event.Lvar("Battery1Switch",1000,"battery")
 function tt_left_pump(varname, value)
     ipc.writeUB(0x66c1, value)
 end
@@ -21,11 +21,7 @@ end
 function tt_available(varname, value)
     ipc.writeUB(0x66c4, value)
 end
--- event.Lvar("TipTanksPresent", 2000, "tt_available")
-function window(varname, value)
-    ipc.writeUB(0x66c5, value)
-end
--- event.Lvar("WindowLeft", 1000, "window")
+event.Lvar("TipTanksPresent", 3000, "tt_available")
 function fan(varname, value)
     ipc.writeUB(0x66c6, value)
 end
@@ -139,6 +135,61 @@ function payload_weight(varname, value)
     ipc.writeUW(0x4222, value)
 end
 event.Lvar("PayloadWeight", 3000, "payload_weight")
+function window_toggle()
+    local wnd = ipc.readLvar("WindowLeft")
+    if wnd == 0 then
+        ipc.writeLvar("WindowLeft", 1)
+    else
+        ipc.writeLvar("WindowLeft", 0)
+    end
+end
+event.key(87, 24, "window_toggle")
+function fsel()
+    local f = ipc.readLvar("FSelBonanzaState")
+    f = f + 1
+    if f > 2 then 
+        f = 0
+    end
+    ipc.writeLvar("FSelBonanzaState", f)
+    ipc.writeUB(0x66c3, f)
+end
+event.key(70, 24, "fsel")
+function left_pump()
+    local var = "TipTankLeftPumpSwitch"
+    local val = ipc.readLvar(var)
+    if val == 0 then
+        ipc.writeLvar(var, 1)
+    else
+        ipc.writeLvar(var, 0)
+    end
+    ipc.writeUB(0x66c1, val)
+end
+event.key(76, 24, "left_pump")
+function right_pump()
+    local var = "TipTankRightPumpSwitch"
+    local val = ipc.readLvar(var)
+    if val == 0 then
+        ipc.writeLvar(var, 1)
+    else
+        ipc.writeLvar(var, 0)
+    end
+    ipc.writeUB(0x66c2, val)
+end
+event.key(82, 24, "right_pump")
 
-
-
+function heat_inc()
+    local t = ipc.readLvar("CabinTempControl")
+    t = t + 10
+    if t > 100 then t = 100 end
+    ipc.writeLvar("CabinTempControl", t)
+    ipc.writeUB(0x66c7, t)
+end
+event.key(72, 24, "heat_inc")
+function heat_dec()
+    local t = ipc.readLvar("CabinTempControl")
+    t = t - 10
+    if t < 0 then t = 0 end
+    ipc.writeLvar("CabinTempControl", t)
+    ipc.writeUB(0x66c7, t)
+end
+event.key(71, 24, "heat_dec")
